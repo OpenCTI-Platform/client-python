d90fae23b5057c7a4d4bab2f8926334b63e209e2
Samuel Hassine <samuel.hassine@ssi.gouv.fr>
2020-01-23 16:50:44 +0100

    [client] Implement files upload (global or in entity) (#50)

---
+++ examples/upload_file_to_intrusion_set.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+
+import datetime
+from pycti import OpenCTIApiClient
+
+# Variables
+api_url = "https://demo.opencti.io"
+api_token = "609caced-7610-4c84-80b4-f3a380d1939b"
+
+# OpenCTI initialization
+opencti_api_client = OpenCTIApiClient(api_url, api_token)
+
+# Create the Intrusion Set
+intrusion_set = opencti_api_client.intrusion_set.create(
+    name="My new Intrusion Set",
+    description="Evil Cluster",
+    first_seen=datetime.date.today().strftime("%Y-%m-%dT%H:%M:%S+00:00"),
+    last_seen=datetime.date.today().strftime("%Y-%m-%dT%H:%M:%S+00:00"),
+    update=True,
+)
+
+# Print
+print(intrusion_set)
+
+# Upload the file
+file = opencti_api_client.stix_domain_entity.upload_file(
+    id=intrusion_set["id"], file_name="./2005_002_001_14428.pdf",
+)
+print(file)
