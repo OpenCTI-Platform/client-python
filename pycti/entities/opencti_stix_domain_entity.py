f879f4dd38c4fdafd1966d6975ad646c62aaad6c
Samuel Hassine <samuel.hassine@ssi.gouv.fr>
2020-01-23 14:54:02 +0100

    [examples] Change token with new demo token

--- pycti/entities/opencti_stix_domain_entity.py
+++ pycti/entities/opencti_stix_domain_entity.py
@@ -67,7 +67,7 @@ class StixDomainEntity:
                         id
                     }
                 }
-            } 
+            }
             externalReferences {
                 edges {
                     node {
