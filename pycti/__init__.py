c2e4b371997729c7d520df45f3ddbd32b377b573
Robert Haist <robert.haist@dcso.de>
2020-04-21 11:32:07 +0200

    Rewrite main __init__.py

--- pycti/__init__.py
+++ pycti/__init__.py
@@ -1,1 +1,71 @@
 # -*- coding: utf-8 -*-
+from .api.opencti_api_client import OpenCTIApiClient
+from .api.opencti_api_connector import OpenCTIApiConnector
+from .api.opencti_api_job import OpenCTIApiJob
+
+from .connector.opencti_connector import ConnectorType
+from .connector.opencti_connector import OpenCTIConnector
+from .connector.opencti_connector_helper import (
+    OpenCTIConnectorHelper,
+    get_config_variable,
+)
+
+from .entities.opencti_tag import Tag
+from .entities.opencti_marking_definition import MarkingDefinition
+from .entities.opencti_external_reference import ExternalReference
+from .entities.opencti_kill_chain_phase import KillChainPhase
+from .entities.opencti_stix_entity import StixEntity
+from .entities.opencti_stix_domain_entity import StixDomainEntity
+from .entities.opencti_stix_observable import StixObservable
+from .entities.opencti_stix_relation import StixRelation
+from .entities.opencti_stix_observable_relation import StixObservableRelation
+from .entities.opencti_identity import Identity
+from .entities.opencti_threat_actor import ThreatActor
+from .entities.opencti_intrusion_set import IntrusionSet
+from .entities.opencti_campaign import Campaign
+from .entities.opencti_incident import Incident
+from .entities.opencti_malware import Malware
+from .entities.opencti_tool import Tool
+from .entities.opencti_vulnerability import Vulnerability
+from .entities.opencti_attack_pattern import AttackPattern
+from .entities.opencti_course_of_action import CourseOfAction
+from .entities.opencti_report import Report
+from .entities.opencti_indicator import Indicator
+
+from .utils.opencti_stix2 import OpenCTIStix2
+from .utils.constants import ObservableTypes
+from .utils.constants import CustomProperties
+
+__all__ = [
+    "OpenCTIApiClient",
+    "OpenCTIApiConnector",
+    "OpenCTIApiJob",
+    "ConnectorType",
+    "OpenCTIConnector",
+    "OpenCTIConnectorHelper",
+    "get_config_variable",
+    "Tag",
+    "MarkingDefinition",
+    "ExternalReference",
+    "KillChainPhase",
+    "StixEntity",
+    "StixDomainEntity",
+    "StixObservable",
+    "StixRelation",
+    "StixObservableRelation",
+    "Identity",
+    "ThreatActor",
+    "IntrusionSet",
+    "Campaign",
+    "Incident",
+    "Malware",
+    "Tool",
+    "Vulnerability",
+    "AttackPattern",
+    "CourseOfAction",
+    "Report",
+    "Indicator",
+    "OpenCTIStix2",
+    "ObservableTypes",
+    "CustomProperties",
+]
