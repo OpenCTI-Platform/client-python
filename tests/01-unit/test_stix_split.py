99024a434219669c28f875b55df7aca0647de38a
Julien Richard <richard.julien@gmail.com>
2020-08-23 18:23:19 +0200

    [client] Improve splitter with a generic approach

--- tests/01-unit/test_stix_split.py
+++ tests/01-unit/test_stix_split.py
@@ -7,4 +7,4 @@ def test_split_bundle():
     with open("./data/enterprise-attack.json") as file:
         content = file.read()
         bundles = stix_splitter.split_bundle(content)
-    assert len(bundles) == 7031
+    assert len(bundles) == 7029
