8733a2a8430e16200cbecf7ea07432259f9f3c94
Robert Haist <rhaist@mailbox.org>
2021-02-11 12:13:41 +0100

    Fix Tests

--- tests/test_indicators.py
+++ tests/test_indicators.py
@@ -34,18 +34,17 @@ def test_indicator(api_client):
         name="C2 server of the new campaign",
         description="This is the C2 server of the campaign",
         pattern_type="stix",
-        indicator_pattern="[domain-name:value = 'www.5z8.info' AND domain-name:resolves_to_refs[*].value = '198.51.100.1/32']",
-        main_observable_type="IPv4-Addr",
+        pattern="[domain-name:value = 'www.5z8.info' AND domain-name:resolves_to_refs[*].value = '198.51.100.1/32']",
+        x_opencti_main_observable_type="IPv4-Addr",
         confidence=60,
-        score=80,
-        detection=True,
+        x_opencti_score=80,
+        x_opencti_detection=True,
         valid_from=date,
         valid_until=date2,
         created=date,
         modified=date,
-        tags=["Test1", "Test2"],
-        createdByRef=organization["id"],
-        markingDefinitions=[
+        createdBy=organization["id"],
+        objectMarking=[
             marking_definition_green["id"],
             marking_definition_white["id"],
         ],
@@ -69,20 +68,20 @@ def test_read_indicator_by_filter(api_client, test_indicator):
         filters=[
             {
                 "key": "name",
-                "values": [test_indicator["name"]],
-                "operator": "eq",
+                "values": ["C2 server of the new campaign"],
             }
         ]
     )
+
     assert indicator2["id"] is not None or indicator2["id"] != ""
     assert indicator2["id"] == test_indicator["id"]
 
 
 def test_get_100_indicators_with_pagination(api_client):
-    # Get all reports using the pagination
+    # Get 100 Indicators using the pagination
     custom_attributes = """
         id
-        indicator_pattern
+        revoked
         created
     """
 
@@ -106,21 +105,7 @@ def test_get_100_indicators_with_pagination(api_client):
     assert len(final_indicators) == 100
 
 
-def test_add_stix_observable_to_indicator(api_client, test_indicator):
-    observable_ttp1 = api_client.stix_observable.create(
-        type="Email-Address",
-        observable_value="phishing@mail.com",
-    )
-
-    assert api_client.indicator.add_stix_observable(
-        id=test_indicator["id"], stix_observable_id=observable_ttp1["id"]
-    )
-
-
-def test_indicator_stix_marshall(api_client, test_indicator):
-    stix_indicator = api_client.indicator.to_stix2(id=test_indicator["id"], mode="full")
-    assert stix_indicator is not None
-
+def test_indicator_stix_marshall(api_client):
     with open("tests/data/indicator_stix.json", "r") as content_file:
         content = content_file.read()
 
