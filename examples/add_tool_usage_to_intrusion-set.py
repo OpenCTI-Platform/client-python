9774da794c2397c66c486364b1dc8c08294bcf70
nor3th <>
2021-06-17 20:38:31 +0200

    [example] Finished writing the example

--- examples/add_tool_usage_to_intrusion-set.py
+++ examples/add_tool_usage_to_intrusion-set.py
@@ -8,3 +8,23 @@ api_token = "YOUR_TOKEN"
 
 # OpenCTI initialization
 opencti_api_client = OpenCTIApiClient(api_url, api_token)
+
+tool = opencti_api_client.tool.create(
+    name="powashell.exe", description="A new evil tool."
+)
+
+intrusion_set = opencti_api_client.intrusion_set.create(
+    name="APT_EVIL"
+)
+
+# Create the relation
+relation = opencti_api_client.stix_core_relationship.create(
+    fromType="IntrusionSet",
+    fromId=intrusion_set["id"],
+    toType="Tool",
+    toId=tool["id"],
+    relationship_type="uses",
+    description="APT_EVIL uses the tool powashell.exe",
+)
+
+print(relation)
