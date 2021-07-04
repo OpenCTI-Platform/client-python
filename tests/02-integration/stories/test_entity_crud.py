2888e113e0446a5fa2d73f74fc7cf984eb41a1b3
nor3th <73965465+nor3th@users.noreply.github.com>
2021-07-04 23:17:49 +0200

    [tests] Added list support for compare_values and other minor changes

--- tests/02-integration/stories/test_entity_crud.py
+++ tests/02-integration/stories/test_entity_crud.py
@@ -13,67 +13,87 @@ class Test_entity_crud:
             if exception_keys and key in exception_keys:
                 continue
 
-            assert (
-                key in retrieved_data
-            ), f"SDO: {sdo}, Key {key} is not in retrieved_data"
+            assert key in retrieved_data, f"{sdo}: Key {key} is not in retrieved_data"
 
             compare_data = retrieved_data[key]
             if isinstance(value, str):
                 assert (
                     value == compare_data
-                ), f"SDO: {sdo}, Key '{key}': {value} does't match {retrieved_data[key]} ({retrieved_data}"
-            # else:
+                ), f"{sdo}: Key '{key}': {value} does't match {retrieved_data[key]} ({retrieved_data}"
+            elif isinstance(value, list):
+                assert len(value) == len(
+                    compare_data
+                ), f"{sdo}: List '{value}' does not have the same length as '{compare_data}'"
+                for value_key in value:
+                    assert (
+                        value_key in compare_data
+                    ), f"{sdo}: List '{compare_data}' does not contain '{value_key}'"
             # TODO compare other values types
 
     def test_create(self, fruit_bowl, api_client):
         for sdo, s_class in fruit_bowl.items():
             s_class.setup()
-            test_indicator = s_class.ownclass().create(**s_class.data())
-            assert test_indicator is not None, f"No ID on object {sdo}"
-            assert "id" in test_indicator, f"No ID on object {sdo}"
+            for class_data in s_class.data():
+                test_indicator = s_class.ownclass().create(**class_data)
+                assert test_indicator is not None, f"{sdo}: No ID on object"
+                assert "id" in test_indicator, f"{sdo}: No ID on object"
 
             s_class.teardown()
 
     def test_read(self, fruit_bowl, api_client):
         for sdo, s_class in fruit_bowl.items():
             s_class.setup()
-            test_indicator = s_class.ownclass().create(**s_class.data())
-            assert test_indicator is not None, f"No ID on object {sdo}"
-            assert "id" in test_indicator, f"No ID on object {sdo}"
-            test_indicator = s_class.ownclass().read(id=test_indicator["id"])
-            self.compare_values(sdo, s_class.data(), test_indicator)
+            for class_data in s_class.data():
+                test_indicator = s_class.ownclass().create(**class_data)
+                assert test_indicator is not None, f"{sdo}: No ID on object"
+                assert "id" in test_indicator, f"{sdo}: No ID on object"
+                test_indicator = s_class.ownclass().read(id=test_indicator["id"])
+                self.compare_values(sdo, class_data, test_indicator)
 
             s_class.teardown()
 
     def test_update(self, fruit_bowl, api_client):
         for sdo, s_class in fruit_bowl.items():
             s_class.setup()
-            obj = s_class.data()
-            test_indicator = s_class.ownclass().create(**obj)
-            assert test_indicator is not None, f"No ID on object {sdo}"
-            assert "id" in test_indicator, f"No ID on object {sdo}"
-            obj["description"] = "Test"
-            obj["update"] = True
-            result = s_class.ownclass().create(**obj)
-            result = s_class.ownclass().read(id=result["id"])
-            assert result["description"] == "Test"
-            assert result["id"] == test_indicator["id"]
+            for class_data in s_class.data():
+                test_indicator = s_class.ownclass().create(**class_data)
+                assert test_indicator is not None, f"{sdo}: No ID on object"
+                assert "id" in test_indicator, f"{sdo}: No ID on object"
+
+                update_field = ""
+                if "description" in class_data:
+                    update_field = "description"
+                elif sdo == "Note":
+                    update_field = "content"
+
+                class_data[update_field] = "Test"
+                class_data["update"] = True
+                result = s_class.ownclass().create(**class_data)
+                result = s_class.ownclass().read(id=result["id"])
+                assert (
+                    result[update_field] == "Test"
+                ), f"{sdo}: Updated field {update_field} is not 'Test', instead{result[update_field]}"
+                assert (
+                    result["id"] == test_indicator["id"]
+                ), f"{sdo}: Updated SDO ID does not match old ID"
 
             s_class.teardown()
 
     def test_delete(self, fruit_bowl, api_client):
-        for sdo, attributes in fruit_bowl.items():
-            attributes.setup()
-            obj = attributes.data()
-            test_indicator = attributes.ownclass().create(**obj)
-            assert test_indicator is not None, f"No ID on object {sdo}"
-            assert "id" in test_indicator, f"No ID on object {sdo}"
-            result = attributes.baseclass().delete(id=test_indicator["id"])
-            assert result is None
-            result = attributes.ownclass().read(id=test_indicator["id"])
-            assert result is None
+        for sdo, s_class in fruit_bowl.items():
+            s_class.setup()
+            for class_data in s_class.data():
+                test_indicator = s_class.ownclass().create(**class_data)
+                assert test_indicator is not None, f"{sdo}: No ID on object"
+                assert "id" in test_indicator, f"{sdo}: No ID on object"
+                result = s_class.baseclass().delete(id=test_indicator["id"])
+                assert result is None, f"{sdo}: Delete returned value '{result}'"
+                result = s_class.ownclass().read(id=test_indicator["id"])
+                assert (
+                    result is None
+                ), f"{sdo}: Read returned value '{result}' after delete"
 
-            attributes.teardown()
+            s_class.teardown()
 
     def test_import_from_stix2(self, fruit_bowl, api_client):
         pass
