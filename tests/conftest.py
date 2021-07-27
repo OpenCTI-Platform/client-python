d87c07f1c91dcdbb4d400e09436a8928216316ea
nor3th <73965465+nor3th@users.noreply.github.com>
2021-07-27 21:47:36 +0200

    [tests] Add switch to enable connector tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import pytest
 from pytest_cases import fixture
 from pycti import OpenCTIApiClient, OpenCTIApiConnector
 
@@ -14,3 +15,22 @@ def api_client():
 @fixture(scope="session")
 def api_connector(api_client):
     return OpenCTIApiConnector(api_client)
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--connectors", action="store_true", default=False, help="run connector tests"
+    )
+
+
+def pytest_configure(config):
+    config.addinivalue_line("markers", "connectors: mark connector tests to run")
+
+
+def pytest_collection_modifyitems(config, items):
+    if config.getoption("--connectors"):
+        return
+    skip_connectors = pytest.mark.skip(reason="need --connectors to run")
+    for item in items:
+        if "connectors" in item.keywords:
+            item.add_marker(skip_connectors)
