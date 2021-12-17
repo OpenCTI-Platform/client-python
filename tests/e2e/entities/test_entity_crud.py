ccf69a4e1bc64ebce26a6e011070efe4589f20fd
nor3th <73965465+nor3th@users.noreply.github.com>
2021-12-17 22:37:30 +0100

    [tests] Moved filter entities test to another file

--- tests/e2e/entities/test_entity_crud.py
+++ tests/e2e/entities/test_entity_crud.py
@@ -75,21 +75,3 @@ def test_delete(entity_class):
     assert result is None, f"Delete returned value '{result}'"
     result = entity_class.own_class().read(id=test_indicator["id"])
     assert result is None, f"Read returned value '{result}' after delete"
-
-
-def test_filter(entity_class):
-    if len(entity_class.get_filter()) == 0:
-        return
-
-    class_data = entity_class.data()
-    test_indicator = entity_class.own_class().create(**class_data)
-    assert test_indicator is not None, "Response is NoneType"
-    assert "id" in test_indicator, "No ID on object"
-    test_indicator = entity_class.own_class().read(filters=entity_class.get_filter())
-    compare_values(
-        class_data,
-        test_indicator,
-        entity_class.get_compare_exception_keys(),
-    )
-
-    entity_class.base_class().delete(id=test_indicator["id"])
