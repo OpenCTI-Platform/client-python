3bf9417eccbc4c31407409680e8bdd4818f86b3e
nor3th <73965465+nor3th@users.noreply.github.com>
2022-06-11 20:45:09 +0200

    [tests] Added connectorv2 tests

---
+++ tests/03_e2e/connector/v2/simple_connector_test.py
@@ -0,0 +1,119 @@
+import json
+import time
+from typing import List, Optional
+
+import requests
+from time import sleep
+
+from pycti.connector.v2.connectors.connector import Connector
+from pycti.connector.v2.libs.orchestrator_schemas import WorkflowCreate, RunCreate, ConfigCreate, Config, Workflow, Run, \
+    JobStatus, State, Result
+from .stix_ingester import StixWorker, TestExternalImport
+
+
+def test_connector_setup_and_heartbeat(schedule_server, monkeypatch, caplog):
+    monkeypatch.setenv('scheduler', schedule_server)
+    connector = StixWorker()
+    sleep(5 + 1)    # 5 = heartbeat delay and 1 to avoid race condition
+    assert "Successful heartbeat OK" in caplog.text, "No heartbeat detected"
+    connector.stop()
+    assert "Successful heartbeat removal" in caplog.text, "Heartbeat removal failed"
+
+
+def test_connector_heartbeat_service(schedule_server, monkeypatch):
+    monkeypatch.setenv('scheduler', schedule_server)
+    # TODO continue here
+    connector = StixWorker()
+    connector_instance = connector.connector_instance
+    response = requests.post(
+        url=f"{schedule_server}/workflow/",
+        json={"connector_instance": connector_instance}
+    )
+    sleep(20)
+    connector.stop()
+
+def get_element(job_status_list: List[JobStatus], config_id: str) -> Optional[JobStatus]:
+    for job_status in job_status_list:
+        if job_status.id == config_id:
+            return job_status
+
+    return None
+
+def test_connector_workflow(schedule_server, monkeypatch):
+    monkeypatch.setenv('scheduler', schedule_server)
+    stix_worker = StixWorker()
+    stix_worker_config = ConfigCreate(
+        connector_id=stix_worker.connector.id,
+        name="StixWorker Config2",
+        config=StixWorker.StixRunConfig(
+            token="18bd74e5-404c-4216-ac74-23de6249d690"
+        )
+    )
+    response = requests.post(f"{schedule_server}/config/", json=stix_worker_config.dict())
+    assert response.status_code == 201
+    stix_config = Config(**json.loads(response.text))
+    external_import = TestExternalImport()
+    external_import_config = ConfigCreate(
+        connector_id=external_import.connector.id,
+        name="EI Import 192.168.14.1",
+        config=TestExternalImport.TestExternalImportRunConfig(
+            ip="192.168.14.1"
+        )
+    )
+    response = requests.post(f"{schedule_server}/config/", json=external_import_config.dict())
+    ei_config = Config(**json.loads(response.text))
+    job_order = [
+            ei_config.id,
+            stix_config.id,
+        ]
+    workflow_create = WorkflowCreate(
+        name="Test Workflow",
+        jobs=job_order,
+        execution_type="triggered",
+        execution_args="",
+        token="123441"
+    )
+    print(workflow_create.json())
+    response = requests.post(f"{schedule_server}/workflow/", json=workflow_create.dict())
+    workflow = Workflow(**json.loads(response.text))
+    run_create = RunCreate(
+        workflow_id=workflow.id,
+        work_id="",
+        applicant_id="",
+        arguments="{}",
+    )
+    response = requests.post(f"{schedule_server}/workflow/{workflow.id}/run", json=run_create.dict())
+    print(response.text)
+    assert response.status_code == 201
+    assert "message" not in json.loads(response.text)
+    run = Run(**json.loads(response.text))
+
+    finished = False
+    while not finished:
+        response = requests.get(f"{schedule_server}/run/{run.id}")
+        assert response.status_code == 200
+        run = Run(**json.loads(response.text))
+        for job in job_order:
+            job_status = get_element(run.job_status, job)
+            print(f"{job} - {job_status}")
+            assert job_status is not None
+
+            status = False
+            if job_status.status == State.finished:
+                assert job_status.result == Result.success
+                status = True
+
+        if run.status == State.finished:
+            assert run.result == Result.success
+            finished = True
+        else:
+            time.sleep(0.5)
+
+    print("Finished")
+    # verify created value
+    stix_worker.stop()
+    external_import.stop()
+
+
+
+
---
+++ tests/03_e2e/connector/v2/stix_ingester.py
@@ -0,0 +1,146 @@
+import json
+
+from pydantic import BaseModel, Extra
+
+from pycti import OpenCTIApiClient, ConnectorType
+from pycti.connector.v2.connectors.connector import Connector
+from stix2 import IPv4Address, Bundle
+
+from pycti.connector.v2.libs.orchestrator_schemas import ConnectorCreate, Config
+
+PROCESSING_COUNT: int = 5
+MAX_PROCESSING_COUNT: int = 30
+
+
+class StixWorker(Connector):
+    class StixRunConfig(BaseModel):
+        token: str
+
+        class Config:
+            extra = Extra.forbid
+
+    def __init__(self):
+        settings = ConnectorCreate(
+            uuid="f005a115-1139-491a-a1e6-7f93971648f3",
+            name="StixWorker2",
+            type="STIX_IMPORT",
+            queue="queue_stix_worker",
+            config_schema=self.StixRunConfig,
+        )
+        super().__init__(settings)
+
+    def run(self, bundle: Bundle, config: StixRunConfig) -> Bundle:
+        print("Started stix worker")
+        api = OpenCTIApiClient(
+            self.environment_config['opencti'],
+            config.token,
+            self.base_config.log_level
+        )
+        bundel = {
+            "type": "bundle",
+            "id": "bundle--177c6477-2dee-43d5-b4c9-8b7f3f5ec517",
+            "objects": [
+                {
+                    "type": "indicator",
+                    "spec_version": "2.1",
+                    "id": "indicator--a862ff86-68d9-42e5-8095-cd80c040e112",
+                    "created": "2020-06-24T15:04:40.048932Z",
+                    "modified": "2020-06-24T15:04:40.048932Z",
+                    "name": "File hash for malware variant",
+                    "pattern": "[file:hashes.md5 = 'd41d8cd98f00b204e9800998ecf8427e']",
+                    "pattern_type": "stix",
+                    "pattern_version": "2.1",
+                    "valid_from": "2020-06-24T15:04:40.048932Z"
+                },
+                {
+                    "type": "malware",
+                    "spec_version": "2.1",
+                    "id": "malware--389c934c-258c-44fb-ae4b-14c6c12270f6",
+                    "created": "2020-06-24T14:53:20.156644Z",
+                    "modified": "2020-06-24T14:53:20.156644Z",
+                    "name": "Poison Ivy",
+                    "is_family": False
+                },
+                {
+                    "type": "relationship",
+                    "spec_version": "2.1",
+                    "id": "relationship--2f6a8785-e27b-487e-b870-b85a2121502d",
+                    "created": "2020-06-24T15:05:18.250605Z",
+                    "modified": "2020-06-24T15:05:18.250605Z",
+                    "relationship_type": "indicates",
+                    "source_ref": "indicator--a862ff86-68d9-42e5-8095-cd80c040e112",
+                    "target_ref": "malware--389c934c-258c-44fb-ae4b-14c6c12270f6"
+                }
+            ]
+        }
+        api.stix2.import_bundle_from_json(
+            json.dumps(bundel), True
+        )
+        return bundle
+
+
+class TestExternalImport(Connector):
+    class TestExternalImportRunConfig(BaseModel):
+        ip: str
+
+        class Config:
+            extra = Extra.forbid
+
+    def __init__(self):
+        settings = ConnectorCreate(
+            uuid="f985a12e-2639-498a-b1e6-7f93971648f1",
+            name="TextExternalImport",
+            type=ConnectorType.EXTERNAL_IMPORT.value,
+            queue="queue_test_external_import",
+            config_schema=self.TestExternalImportRunConfig,
+
+        )
+        super().__init__(settings)
+
+    def run(self, bundle: Bundle, config: TestExternalImportRunConfig) -> Bundle:
+        ip = IPv4Address(value=config.ip)
+        bundle = Bundle(ip)
+        return bundle
+
+    # def run(self, message: str, data: Dict, delivery_tag):
+    #     applicant_id = data.get("applicant_id", None)
+    #     if applicant_id is None:
+    #         raise ValueError("applicant_id empty")
+    #     self.api.set_applicant_id_header(applicant_id)
+    #     work_id = data.get("work_id", None)
+    #     # Execute the import
+    #     processing_count = 1
+    #     try:
+    #         content = message
+    #         tmp_types = data.get("entities_types", [])
+    #         types = tmp_types if len(tmp_types) > 0 else None
+    #         update = data.get("update", False)
+    #         # processing_count = self.processing_count
+    #         if processing_count == PROCESSING_COUNT:
+    #             processing_count = None  # type: ignore
+    #
+    #         self.api.stix2.import_bundle_from_json(
+    #             content, update, types, processing_count
+    #         )
+    #
+    #         if work_id is not None:
+    #             self.api.work.report_expectation(work_id, None)
+    #         self.processing_count = 0
+    #     except Timeout as te:
+    #         self.logger.warning(f"A connection timeout occurred: {{ {te} }}")
+    #         # Platform is under heavy load: wait for unlock & retry almost indefinitely.
+    #         sleep_jitter = round(random.uniform(10, 30), 2)
+    #         time.sleep(sleep_jitter)
+    #         # TODO initiate rerun!
+    #     except RequestException as re:
+    #         # https://docs.python-requests.org/en/master/_modules/requests/exceptions/
+    #         self.logger.error(f"A connection error occurred: {{ {re} }}")
+    #         time.sleep(60)
+    #         self.logger.info(
+    #             f"Message (delivery_tag={delivery_tag}) NOT acknowledged"
+    #         )
+    #         # TODO figure out what to do here, Rerun or simple error?
+    #         # cb = functools.partial(self.nack_message, channel, delivery_tag)
+    #         # connection.add_callback_threadsafe(cb)
+    #     # TODO handle exceptions from here opencti-platform/graphql/src/config/error.js
+    #
