2d193f10c67ac20151a6e2c3b1505f9e27f2936c
Raymond <daemitus@gmail.com>
2022-08-27 18:41:49 -0400

    Another shot at circular imports

--- pycti/entities/opencti_attack_pattern.py
+++ pycti/entities/opencti_attack_pattern.py
@@ -1,5 +1,7 @@
 """OpenCTI Attack-Pattern operations"""
 
+from __future__ import annotations
+
 import json
 import logging
 from datetime import datetime
--- pycti/entities/opencti_tool.py
+++ pycti/entities/opencti_tool.py
@@ -1,5 +1,7 @@
 """OpenCTI Tool operations"""
 
+from __future__ import annotations
+
 import json
 import logging
 from datetime import datetime
