d214cf0149e2fe385d0938e6ce0bf2915473d256
nor3th <73965465+nor3th@users.noreply.github.com>
2022-10-26 13:10:07 +0200

    [tests] Update path

--- pycti/connector/tests/test_library.py
+++ pycti/connector/tests/test_library.py
@@ -10,11 +10,11 @@ def wait_for_test_to_finish(connector_test_instance, old_state: Dict) -> str:
     error = ""
     while not finished:
         new_state = connector_test_instance.connector_instance.get_state()
-        if new_state.get("last_run", None) != old_state.get("last_run", None):
-            finished = True
-
         error = new_state.get("error", "")
         if error:
+            finished = True
+
+        if new_state.get("last_run", None) != old_state.get("last_run", None):
             finished = True
 
         time.sleep(0.5)
