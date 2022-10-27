5ba6f5f74eedd1fb4b7f6ed38d2b99235c50164f
nor3th <73965465+nor3th@users.noreply.github.com>
2022-10-27 16:01:16 +0200

    [connector] Add test timeout

--- pycti/connector/tests/test_library.py
+++ pycti/connector/tests/test_library.py
@@ -2,12 +2,16 @@ import time
 from typing import Dict
 
 
-def wait_for_test_to_finish(connector_test_instance, old_state: Dict) -> str:
+def wait_for_test_to_finish(
+    connector_test_instance, old_state: Dict, timeout: int = 60
+) -> str:
     connector_test_instance.initiate()
+    sleep_delay = 0.5
+    time_spent = 0
 
     finished = False
     error = ""
-    while not finished:
+    while not finished and time_spent < timeout:
         new_state = connector_test_instance.connector_instance.get_state()
         error = new_state.get("error", "")
         if error:
@@ -16,6 +20,10 @@ def wait_for_test_to_finish(connector_test_instance, old_state: Dict) -> str:
         if new_state.get("last_run", None) != old_state.get("last_run", None):
             finished = True
 
-        time.sleep(0.5)
+        time.sleep(sleep_delay)
+        time_spent += sleep_delay
+
+    if time_spent >= timeout and error == "":
+        error = f"Timeout after {time_spent} seconds. Please step through your code to find the issue if no error is visible."
 
     return error
