7f4acf6e5cf155af1e17104bfd62a8998e909717
Julien Richard <julien.richard@filigran.io>
2023-07-06 02:48:01 +0200

    [client] Adapt after rebase

--- pycti/entities/opencti_threat_actor.py
+++ pycti/entities/opencti_threat_actor.py
@@ -1,15 +1,316 @@
+# coding: utf-8
 
+import json
+import uuid
+from typing import Union
+
+from stix2.canonicalization.Canonicalize import canonicalize
+
+from pycti.entities import LOGGER
 from pycti.entities.opencti_threat_actor_group import ThreatActorGroup
 from pycti.entities.opencti_threat_actor_individual import ThreatActorIndividual
+
+
 class ThreatActor:
     """Main ThreatActor class for OpenCTI
 
     :param opencti: instance of :py:class:`~pycti.api.opencti_api_client.OpenCTIApiClient`
     """
+
     def __init__(self, opencti):
         """Create an instance of ThreatActor"""
 
         self.opencti = opencti
+        self.threat_actor_group = ThreatActorGroup(opencti)
+        self.properties = """
+            id
+            standard_id
+            entity_type
+            parent_types
+            spec_version
+            created_at
+            updated_at
+            createdBy {
+                ... on Identity {
+                    id
+                    standard_id
+                    entity_type
+                    parent_types
+                    spec_version
+                    identity_class
+                    name
+                    description
+                    roles
+                    contact_information
+                    x_opencti_aliases
+                    created
+                    modified
+                    objectLabel {
+                        edges {
+                            node {
+                                id
+                                value
+                                color
+                            }
+                        }
+                    }
+                }
+                ... on Organization {
+                    x_opencti_organization_type
+                    x_opencti_reliability
+                }
+                ... on Individual {
+                    x_opencti_firstname
+                    x_opencti_lastname
+                }
+            }
+            objectMarking {
+                edges {
+                    node {
+                        id
+                        standard_id
+                        entity_type
+                        definition_type
+                        definition
+                        created
+                        modified
+                        x_opencti_order
+                        x_opencti_color
+                    }
+                }
+            }
+            objectLabel {
+                edges {
+                    node {
+                        id
+                        value
+                        color
+                    }
+                }
+            }
+            externalReferences {
+                edges {
+                    node {
+                        id
+                        standard_id
+                        entity_type
+                        source_name
+                        description
+                        url
+                        hash
+                        external_id
+                        created
+                        modified
+                        importFiles {
+                            edges {
+                                node {
+                                    id
+                                    name
+                                    size
+                                    metaData {
+                                        mimetype
+                                        version
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            revoked
+            confidence
+            created
+            modified
+            name
+            description
+            aliases
+            threat_actor_types
+            first_seen
+            last_seen
+            roles
+            goals
+            sophistication
+            resource_level
+            primary_motivation
+            secondary_motivations
+            importFiles {
+                edges {
+                    node {
+                        id
+                        name
+                        size
+                        metaData {
+                            mimetype
+                            version
+                        }
+                    }
+                }
+            }
+        """
+
+    @staticmethod
+    def generate_id(name):
+        name = name.lower().strip()
+        data = {"name": name}
+        data = canonicalize(data, utf8=False)
+        id = str(uuid.uuid5(uuid.UUID("00abedb4-aa42-466c-9c01-fed23315a9b7"), data))
+        return "threat-actor--" + id
+
+    def list(self, **kwargs) -> dict:
+        """List Threat-Actor objects
+
+        The list method accepts the following kwargs:
+
+        :param list filters: (optional) the filters to apply
+        :param str search: (optional) a search keyword to apply for the listing
+        :param int first: (optional) return the first n rows from the `after` ID
+                            or the beginning if not set
+        :param str after: (optional) OpenCTI object ID of the first row for pagination
+        :param str orderBy: (optional) the field to order the response on
+        :param bool orderMode: (optional) either "`asc`" or "`desc`"
+        :param bool getAll: (optional) switch to return all entries (be careful to use this without any other filters)
+        :param bool withPagination: (optional) switch to use pagination
+        """
+
+        filters = kwargs.get("filters", None)
+        search = kwargs.get("search", None)
+        first = kwargs.get("first", 500)
+        after = kwargs.get("after", None)
+        order_by = kwargs.get("orderBy", None)
+        order_mode = kwargs.get("orderMode", None)
+        custom_attributes = kwargs.get("customAttributes", None)
+        get_all = kwargs.get("getAll", False)
+        with_pagination = kwargs.get("withPagination", False)
+        if get_all:
+            first = 500
+
+        LOGGER.info("Listing Threat-Actors with filters %s.", json.dumps(filters))
+        query = (
+            """
+                query ThreatActors($filters: [ThreatActorsFiltering], $search: String, $first: Int, $after: ID, $orderBy: ThreatActorsOrdering, $orderMode: OrderingMode) {
+                    threatActors(filters: $filters, search: $search, first: $first, after: $after, orderBy: $orderBy, orderMode: $orderMode) {
+                        edges {
+                            node {
+                                """
+            + (custom_attributes if custom_attributes is not None else self.properties)
+            + """
+                        }
+                    }
+                    pageInfo {
+                        startCursor
+                        endCursor
+                        hasNextPage
+                        hasPreviousPage
+                        globalCount
+                    }
+                }
+            }
+        """
+        )
+        result = self.opencti.query(
+            query,
+            {
+                "filters": filters,
+                "search": search,
+                "first": first,
+                "after": after,
+                "orderBy": order_by,
+                "orderMode": order_mode,
+            },
+        )
+        return self.opencti.process_multiple(
+            result["data"]["threatActors"], with_pagination
+        )
+
+    def read(self, **kwargs) -> Union[dict, None]:
+        """Read a Threat-Actor object
+
+        read can be either used with a known OpenCTI entity `id` or by using a
+        valid filter to search and return a single Threat-Actor entity or None.
+
+        The list method accepts the following kwargs.
+
+        Note: either `id` or `filters` is required.
+
+        :param str id: the id of the Threat-Actor
+        :param list filters: the filters to apply if no id provided
+        """
+
+        id = kwargs.get("id", None)
+        filters = kwargs.get("filters", None)
+        custom_attributes = kwargs.get("customAttributes", None)
+        if id is not None:
+            LOGGER.info("Reading Threat-Actor {%s}.", id)
+            query = (
+                """
+                    query ThreatActor($id: String!) {
+                        threatActor(id: $id) {
+                            """
+                + (
+                    custom_attributes
+                    if custom_attributes is not None
+                    else self.properties
+                )
+                + """
+                    }
+                }
+             """
+            )
+            result = self.opencti.query(query, {"id": id})
+            return self.opencti.process_multiple_fields(result["data"]["threatActor"])
+        elif filters is not None:
+            result = self.list(filters=filters)
+            if len(result) > 0:
+                return result[0]
+            else:
+                return None
+        else:
+            LOGGER.error("[opencti_threat_actor] Missing parameters: id or filters")
+            return None
+
+    def create(self, **kwargs):
+        """Create a Threat-Actor object
+
+        The Threat-Actor entity will only be created if it doesn't exists
+        By setting `update` to `True` it acts like an upsert and updates
+        fields of an existing Threat-Actor entity.
+
+        The create method accepts the following kwargs.
+
+        Note: `name` and `description` or `stix_id` is required.
+
+        :param str stix_id: stix2 id reference for the Threat-Actor entity
+        :param str createdBy: (optional) id of the organization that created the knowledge
+        :param list objectMarking: (optional) list of OpenCTI markin definition ids
+        :param list objectLabel: (optional) list of OpenCTI label ids
+        :param list externalReferences: (optional) list of OpenCTI external references ids
+        :param bool revoked: is this entity revoked
+        :param int confidence: confidence level
+        :param str lang: language
+        :param str created: (optional) date in OpenCTI date format
+        :param str modified: (optional) date in OpenCTI date format
+        :param str name: name of the threat actor
+        :param str description: description of the threat actor
+        :param list aliases: (optional) list of alias names for the Threat-Actor
+        :param list threat_actor_types: (optional) list of threat actor types
+        :param str first_seen: (optional) date in OpenCTI date format
+        :param str last_seen: (optional) date in OpenCTI date format
+        :param list roles: (optional) list of roles
+        :param list goals: (optional) list of goals
+        :param str sophistication: (optional) describe the actors sophistication in text
+        :param str resource_level: (optional) describe the actors resource_level in text
+        :param str primary_motivation: (optional) describe the actors primary_motivation in text
+        :param list secondary_motivations: (optional) describe the actors secondary_motivations in list of string
+        :param bool update: (optional) choose to updated an existing Threat-Actor entity, default `False`
+        """
+        return self.threat_actor_group.create(**kwargs)
+
+    """
+        Import an Threat-Actor object from a STIX2 object
+
+        :param stixObject: the Stix-Object Intrusion-Set
+        :return Intrusion-Set object
+    """
 
     def import_from_stix2(self, **kwargs):
         stix_object = kwargs.get("stixObject", None)
@@ -23,6 +324,6 @@ class ThreatActor:
             type = "threat-actor-group"
 
         if "threat-actor-group" in type:
-            return ThreatActorGroup.import_from_stix2(self, kwargs)
+            return ThreatActorGroup.import_from_stix2(**kwargs)
         if "threat-actor-individual" in type:
-            return ThreatActorIndividual.import_from_stix2(self, kwargs)
+            return ThreatActorIndividual.import_from_stix2(**kwargs)
