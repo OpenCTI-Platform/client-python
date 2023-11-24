8dd2892822fe244fbc578980a67b7818ede0619e
Cas Obdam <17879738+Obdam@users.noreply.github.com>
2023-11-24 13:40:28 +0100

    Added docstrings, type hints

--- pycti/api/opencti_api_authentication.py
+++ pycti/api/opencti_api_authentication.py
@@ -1,14 +1,39 @@
+from typing import Dict, Optional, Tuple, Union
+
 import requests
 
 
 class OpenCTIApiAuthentication(requests.auth.AuthBase):
+    """
+    Custom authentication class for OpenCTI API requests.
+
+    This class is used to add necessary configuration to the
+    requests sent to the OpenCTI API. It extends requests'
+    AuthBase to integrate with the requests library.
+
+    Attributes:
+        pycti_version (str): Version of the pycti client.
+        bearer_token (str): Bearer token for authentication.
+        proxies (Optional[Dict[str, str]]): Proxies configuration.
+        verify (Union[bool, str]): SSL certificate verification setting.
+        cert (Optional[Union[str, Tuple[str, str]]]): Client-side certificates.
+        headers (Dict[str, str]): Headers to include in each request.
+
+    Parameters:
+        bearer_token (str): Bearer token for OpenCTI API authentication.
+        pycti_version (str): The version of the pycti client being used.
+        proxies (Optional[Dict[str, str]]): Proxies to use for the requests.
+        verify (Union[bool, str]): Whether to verify SSL certificates or a path to CA bundle.
+        cert (Optional[Union[str, Tuple[str, str]]]): Client-side certificates.
+    """
+
     def __init__(
         self,
-        bearer_token,
-        pycti_version,
-        proxies,
-        verify,
-        cert,
+        bearer_token: str,
+        pycti_version: str,
+        proxies: Optional[Dict[str, str]],
+        verify: Union[bool, str],
+        cert: Optional[Union[str, Tuple[str, str]]],
     ):
         super().__init__()
         self.pycti_version = pycti_version
@@ -21,7 +46,16 @@ class OpenCTIApiAuthentication(requests.auth.AuthBase):
             "Authorization": "Bearer " + self.bearer_token,
         }
 
-    def __call__(self, r):
+    def __call__(self, r) -> requests.Request:
+        """
+        Update the request with authentication headers, proxies, and SSL settings.
+
+        Parameters:
+            r (requests.Request): The request object to be modified.
+
+        Returns:
+            requests.Request: The modified request object.
+        """
         r.headers.update(self.headers)
         r.proxies = self.proxies
         r.verify = self.verify
@@ -30,6 +64,16 @@ class OpenCTIApiAuthentication(requests.auth.AuthBase):
         return r
 
     def set_custom_header(self, key: str, value: str):
+        """
+        Set or update a custom header for the requests.
+
+        Parameters:
+            key (str): The header name.
+            value (str): The header value.
+
+        Raises:
+            ValueError: If either key or value is not a string.
+        """
         if key is not None and value is not None:
             self.headers.update({key: value})
         else:
