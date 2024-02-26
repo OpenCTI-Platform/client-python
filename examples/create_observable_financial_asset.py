551400c4da47d3e2b65b1ee67d26925cf25eb3af
Bonsai8863 <131906254+Bonsai8863@users.noreply.github.com>
2024-02-26 16:02:40 -0500

    financial observable creation examples

---
+++ examples/create_observable_financial_asset.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+
+from pycti import OpenCTIApiClient
+
+# Variables
+api_url = "http://localhost:4000"
+api_token = "6C2C9EAE-6FF5-4421-B118-74A3CA5AAC20"
+
+# OpenCTI initialization
+opencti_api_client = OpenCTIApiClient(api_url, api_token)
+
+process = opencti_api_client.stix_cyber_observable.create(
+    observableData={
+        "type": "Financial-Asset",
+        "name": "Joe's Big Boat",
+        "asset_type": "boat",
+        "asset_value": 12000000,
+        "currency_code": "belarusian_ruble_(byr)",
+    }
+)
+
+print(process)
