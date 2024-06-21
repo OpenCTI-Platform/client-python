9f92b55799b1cc94629142652464a3980f6beacc
marie flores <marie.flores@filigran.io>
2024-06-21 11:44:54 +0200

    [client] fix workflow_id typo

--- pycti/entities/opencti_identity.py
+++ pycti/entities/opencti_identity.py
@@ -543,7 +543,7 @@ class Identity:
             if "x_opencti_workflow_id" not in stix_object:
                 stix_object["x_opencti_workflow_id"] = (
                     self.opencti.get_attribute_in_extension(
-                        "x_opencti_workflow_id", stix_object
+                        "workflow_id", stix_object
                     )
                 )
 
