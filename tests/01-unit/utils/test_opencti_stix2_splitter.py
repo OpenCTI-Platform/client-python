3415023cd028c35eaceb9703dae392463823cd70
Julien Richard <julien.richard@filigran.io>
2024-09-20 00:39:44 +0200

    [client] Improve splitter to deduplicate and prevent any infinite recursion

--- tests/01-unit/utils/test_opencti_stix2_splitter.py
+++ tests/01-unit/utils/test_opencti_stix2_splitter.py
@@ -1,3 +1,4 @@
+import json
 import uuid
 
 from stix2 import Report
@@ -10,7 +11,23 @@ def test_split_bundle():
     with open("./tests/data/enterprise-attack.json") as file:
         content = file.read()
     expectations, bundles = stix_splitter.split_bundle_with_expectations(content)
-    assert expectations == 7029
+    assert expectations == 7016
+
+
+def test_split_mono_bundle():
+    stix_splitter = OpenCTIStix2Splitter()
+    with open("./tests/data/mono-bundle.json") as file:
+        content = file.read()
+    expectations, bundles = stix_splitter.split_bundle_with_expectations(content)
+    assert expectations == 1
+
+
+def test_split_capec_bundle():
+    stix_splitter = OpenCTIStix2Splitter()
+    with open("./tests/data/mitre_att_capec.json") as file:
+        content = file.read()
+    expectations, bundles = stix_splitter.split_bundle_with_expectations(content)
+    assert expectations == 2610
 
 
 def test_split_cyclic_bundle():
@@ -18,7 +35,17 @@ def test_split_cyclic_bundle():
     with open("./tests/data/cyclic-bundle.json") as file:
         content = file.read()
     expectations, bundles = stix_splitter.split_bundle_with_expectations(content)
-    assert expectations == 3
+    assert expectations == 6
+    for bundle in bundles:
+        json_bundle = json.loads(bundle)
+        object_json = json_bundle["objects"][0]
+        if object_json["id"] == "report--a445d22a-db0c-4b5d-9ec8-e9ad0b6dbdd7":
+            assert (
+                len(object_json["external_references"]) == 1
+            )  # References are duplicated
+            assert len(object_json["object_refs"]) == 2  # Cleaned cyclic refs
+            assert len(object_json["object_marking_refs"]) == 1
+            assert object_json["object_marking_refs"][0] == "marking-definition--78ca4366-f5b8-4764-83f7-34ce38198e27"
 
 
 def test_create_bundle():
