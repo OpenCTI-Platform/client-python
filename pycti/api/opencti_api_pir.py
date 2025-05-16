e4e48f98fb4a7bfcca2c892e5d9bf81901c70818
Cathia Archidoit <cathia.archidoit@filigran.io>
2025-05-16 09:18:29 +0200

    [client] fix

--- pycti/api/opencti_api_pir.py
+++ pycti/api/opencti_api_pir.py
@@ -8,7 +8,7 @@ class OpenCTIApiPir:
         id = kwargs.get("id", None)
         input = kwargs.get("input", None)
         query = """
-            mutation PirAddDependency($id: ID!, $input: PirAddDependencyInput!) {
+            mutation PirAddDependency($id: ID!, $input: PirDependencyAddInput!) {
                 pirAddDependency(id: $id, input: $input)
             }
            """
